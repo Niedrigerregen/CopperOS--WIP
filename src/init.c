1f6deed150ca9460e4394a34f5eef6e1ee0b64de
Loran <yeetnonsens123@gmail.com>
2026-03-25 17:39:19 +0100

    Refactor code structure

--- src/init.c
+++ src/init.c
@@ -1,5 +1,5 @@
-//turns out it's pretty difficult to make an init = shell system
-
+//turns out it's pretty difficult to make an init = shell system. Or is it? I forgot to set the console in grub.cfg so maybe that's why it didn't work
+//idk man do i look like some coding senior i'm an awkward 14 year old
 
 #include <unistd.h>
 
